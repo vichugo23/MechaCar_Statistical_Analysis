d351770e05e59a26c31896d6c799ecbbb49ed2a2
vichugo23 <110702997+vichugo23@users.noreply.github.com>
2022-11-24 01:18:02 -0600

    Update MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,7 +3,8 @@ library(dplyr)
 MechaCarMPGdf <- read.csv(file="MechaCar_mpg.csv", check.names=F, stringsAsFactors = F)
 
 
-lm(vehicle_length~vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,MechaCarMPGdf)
+lm(formula= mpg ~vehicle_length+ vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,MechaCarMPGdf)
 
-summary(lm(vehicle_length~vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,MechaCarMPGdf)
-        + ) #generate summary statistics
+summary(lm(mpg~vehicle_length+ vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,MechaCarMPGdf))
+#generate summary statistics
+
